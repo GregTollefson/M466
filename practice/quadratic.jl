08d534e4169f67143420f0d1ec00b7038b8c5599
Greg Tollefson <gregtollefson@gmail.com>
2026-08-22 10:22:06 -0700

    small test code to practice workflow

---
+++ practice/quadratic.jl
@@ -0,0 +1,8 @@
+function quatratic(a, b, c)
+	a == 0 && throw(DomainError(a, "a must be nonzero for a quadratic polynomial"))
+
+	discriminant = b^2 - 4a*c
+	discriminant > 0 || throw(DomainError(discriminant, "the discriminant must be positive"))
+
+	return ((-b + sqrt(discriminant)) / (2a), (-b - sqrt(discriminant)) / (2a))
+end
